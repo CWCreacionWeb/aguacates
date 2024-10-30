06e10c3fe32285490f36c1b0cb605de1d24f3802
Guillermo <guillevazquez79@gmail.com>
2024-10-30 10:13:51 +0100

    ACT

---
+++ tools/ToolsClass.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+
+class tools:
+
+    def __init__(self,marker,color):
+        self.color = color
+        self.marker = marker
+
+    def date_format(date):
+        return pd.to_datetime(date)
