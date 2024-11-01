bddbdf745a9261b1709f3b9f1ecea426c26a09a9
Guillermo <guillevazquez79@gmail.com>
2024-11-01 11:02:49 +0100

    ACT

--- Models/Charts.py
+++ Models/Charts.py
@@ -13,14 +13,24 @@ class Charts:
     def __init__(self, file):
         data = pd.read_csv(file)
         self.df = pd.DataFrame(data)
-        plt.figure(figsize=(10, 5))
+        self.figureConfig()
         self.seasonal_decompose = seasonal_decompose
 
-    def figureConfig(self, width=12, height=6):
+    def figureConfig(self, width=11, height=6,**kwargs):
         plt.figure(figsize=(width, height))
+        plt.grid(True)
+        # Uso kwargs directamente en plt para pasar etiquetas, leyendas, etc.
+        if kwargs:
+            plt.gca().set(**kwargs)
 
-    def showData(self):
-        return self.df
+    def showData(self,key = None):
+        if key:
+            return self.df[key]
+        else:
+            return self.df
+
+    def selectedKeys(self, keys):
+        return self.df[keys]
 
     def makeFrame(data):
         return pd.DataFrame(data)
@@ -37,11 +47,11 @@ class Charts:
     def formatDate(self, column_name):
         self.df[column_name] = pd.to_datetime(self.df[column_name])
 
-    def plot(self, x, y, title=None, xlabel=None, ylabel=None):
-        plt.plot(x, y)
-        plt.title(title)
-        plt.xlabel(xlabel)
-        plt.ylabel(ylabel)
+    def plot(self, x, y):
+        plt.plot(x, y,marker='o')
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
         plt.show()
 
 
@@ -72,4 +82,19 @@ class Charts:
         ax.set(xlabel=xlabel, ylabel=ylabel, title=title)
         ax.grid()
         plt.show()
+
+    def temporada(fecha) :
+        #COmprobamos a que estacion del año pertenece la fecha
+        #obtenemos el mes de la fecha dada
+        fecha = pd.to_datetime(fecha) #convertimos la fecha a formato datetime
+
+        if fecha.month in [12, 1, 2]:
+            return 'Winter'
+        elif fecha.month in [3, 4, 5]:
+            return 'Spring'
+        elif fecha.month in [6, 7, 8]:
+            return 'Summer'
+        else:
+            return 'Autumn'
+
 
